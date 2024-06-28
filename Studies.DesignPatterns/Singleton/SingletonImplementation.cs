1aa0321a688f87bdbaacca3a10caddc3709aee43
Joaquim Candido <joaquim.candido@eldorado.org.br>
2024-06-28 19:42:07 -0300

    More

---
+++ Studies.DesignPatterns/Singleton/SingletonImplementation.cs
@@ -0,0 +1,15 @@
+﻿namespace Studies.DesignPatterns.Singleton;
+
+public class SingletonImplementation
+{
+    private static SingletonImplementation? Instance = null;
+
+    public string? TestProperty { get; set; }
+
+    private SingletonImplementation() { }
+
+    public static SingletonImplementation GetInstance()
+    {
+        return Instance ??= Instance = new SingletonImplementation();
+    }
+}
